f58e10c277bb75f211f02af3a78d99db626d2221
Gregory <jamesofin@gmail.com>
2022-06-07 16:37:32 +0500

    release mobile ver site

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ $(function(){
         arrows: false
     });
     var mixer = mixitup('.gallery__items');
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
 });
+
+
